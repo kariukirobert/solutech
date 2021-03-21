0f870b1f8fa47670c243d8bc443f3c1180e8852c
kariukirobert <robmainah@gmail.com>
2021-03-21 16:49:01 +0300

    create migrations

---
+++ database/migrations/2021_03_21_131332_create_supplier_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSupplierProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('supplier_products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('supply_id')->constrained('orders')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('supplier_products');
+    }
+}
