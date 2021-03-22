59d7366e16a0e5407f9f71402f1077a46933aa15
kariukirobert <robmainah@gmail.com>
2021-03-22 09:37:44 +0300

    add routes

---
+++ resources/js/routes.vue
@@ -0,0 +1,30 @@
+<script>
+
+import Products from "./components/products.vue";
+import Suppliers from "./components/suppliers.vue";
+import Orders from "./components/orders.vue";
+
+export default [
+	{
+		path: "/",
+		name: "home",
+		component: Products
+	},
+	{
+		path: "/products",
+		name: "products",
+		component: Products
+	},
+	{
+		path: "/suppliers",
+		name: "suppliers",
+		component: Suppliers
+	},
+	{
+		path: "/orders",
+		name: "orders",
+		component: Orders
+	},
+]
+
+</script>
